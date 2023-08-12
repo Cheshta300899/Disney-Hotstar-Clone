c01c0b87d0ac6df411035fd3fb5e42bfefdaee9f
Cheshta Gupta <85059628+Cheshta300899@users.noreply.github.com>
2023-08-12 19:06:43 +0530

    Add files via upload

---
+++ assets/js/data.js
@@ -0,0 +1,32 @@
+let movies = [
+  {
+    name: "falcon and the winter soldier",
+    des:
+      "Following the events of Avengers: Endgame, Sam Wilson and Bucky Barnes team up in a global adventure that tests their abilities and their patience.",
+    image: "assets/img/slider 2.png"
+  },
+  {
+    name: "loki",
+    des:
+      "The mercurial villain Loki resumes his role as the God of Mischief in a new series that takes place after the events of Avengers Endgame.",
+    image: "assets/img/slider 1.png"
+  },
+  {
+    name: "wanda vision",
+    des:
+      "Living idealized suburban lives, super-powered beings Wanda and Vision begin to suspect that everything is not as it seems.",
+    image: "assets/img/slider 3.png"
+  },
+  {
+    name: "raya and the last dragon",
+    des:
+      "Raya, a warrior, sets out to track down Sisu, a dragon, who transferred all her powers into a magical gem which is now scattered all over the kingdom of Kumandra, dividing its people.",
+    image: "assets/img/slider 4.png"
+  },
+  {
+    name: "luca",
+    des:
+      "Set in a beautiful seaside town on the Italian Riviera, the original animated feature is a coming-of-age story about one young boy experiencing an unforgettable summer filled with gelato, pasta and endless scooter rides. ",
+    image: "assets/img/slider 5.png"
+  }
+];
